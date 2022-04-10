951a0c38234600faa066fcf359d10cba330e5a8a
Shubham Rawat <shubhamrwt2001@gmail.com>
2022-04-10 20:48:08 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import { useState } from 'react';
+import About from "./components/About"
+import Alert from './components/Alert';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
 const title = "TEXT PLZ";
 const about = "About"
-const heading = "Enter your Text";
+
 function App() {
-  
+  const [mode,setMode] = useState("light");
+  const [modeInv,setModeInv] = useState("dark");
+  const [alert,setAlert] = useState(null);
+  const handleMode=()=>{
+    if (mode==="light"){
+      setMode("dark");
+      setModeInv("light")
+      document.body.style.backgroundColor = "#212121";
+      showAlert("Dark mode has been enabled","success");
+    }
+    else{
+      setMode("light");
+      setModeInv("dark");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled","success");
+    }
+  }
+
+  const showAlert = (msg,type)=>{
+    setAlert({
+      message:msg,
+      type:type
+    })
+  }
+
   return (<>
-    <Navbar title={title}  about={about}/>
-    <div className="container">
-    <Textform heading={heading}/>
+   <Router>
+    <Navbar title={title}  about={about} mode={mode} modeInv={modeInv} handlemode={handleMode}/>
+    <Alert alert={alert} />
+    <div className="container"  >
+     
+    <Switch>
+      <Route path="/about">
+        <About/>
+      </Route>
+      <Route path="/">
+        <Textform mode={mode}/>
+      </Route>
+    </Switch>
+    
+      
+    
     </div>
+    </Router>
     </>
+  
   );
 }
 
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,21 +1,76 @@
 import React ,{useState} from 'react'
+import deepai from "deepai";
 
 export default function Textform(props) {
-    const [text,setText] = useState("Enter Text");
-   
+    const [text,setText] = useState("");
+    const [respText,setRespText] =useState("");
+    const computeWord= (text)=>{
+        
+        const wordsArray = text.split(" ");
+        let words_count = wordsArray.filter((word)=>word!=='').length;
+        return words_count
+    }
+    const removeSpaces=()=>{
+        let wordsArray = text.split(" ");
+        wordsArray = wordsArray.filter((word)=>word!=='').filter((word)=>word!=='.');
+        setText(wordsArray.join(" "))
+    }
+    
+    const summarize=async(text)=>{
+        deepai.setApiKey('2aef5b19-2ad3-47cc-b684-9ebca46fda08');
+        var resp = await deepai.callStandardApi("summarization", {
+            text: text,
+        });
+        console.log(text)
+        setRespText(resp.output);
+        console.log(resp.output);
+    }
+    const computeCharaters=(text)=>{
+        let charArray = text.split("");
+        charArray = charArray.filter((word)=>word!==' ');
+        return charArray.length;
+    }
+    const gen_Text=async(text)=>{
+        deepai.setApiKey('2aef5b19-2ad3-47cc-b684-9ebca46fda08');
+        var resp = await deepai.callStandardApi("text-generator", {
+            text: text,
+        });
+        console.log(text)
+        setRespText(resp.output);
+        console.log(resp.output);
+    }
+    const copyResponse = ()=>{
+        navigator.clipboard.writeText(respText);
+
+    }
     return (
-        <div>   
+        <>
+        <div className="container" style={{color: props.mode==="light"?"black":"white" }}>   
             <h1>
-                {props.heading} - {text}
+                {props.heading} 
             </h1>
-                <div className="mb-3">
+            <div className="mb-3">
                 <label htmlFor="myBox" className="form-label" ></label>
-                <textarea className="form-control" id="myBox" rows="8" value={text} onChange={(e)=>{setText(e.target.value)}}></textarea>
-                {console.log(text)}
+                <textarea className="form-control"   style={{backgroundColor: props.mode==="light"?"white":"#212121",color: props.mode==="light"?"black":"white" }} id="myBox" rows="8" value={text} onChange={(e)=>{setText(e.target.value)}}></textarea>
+                
             </div>
             
-            <button type="button" className="btn btn-primary" onClick={()=>{setText(text.toUpperCase())}}>Convert To Upper</button>
+            <button type="button" className="btn btn-primary mx-2 my-3 " onClick={()=>{setText(text.toUpperCase())}}>Convert To Upper</button>
+            <button type="button" className="btn btn-primary mx-2 my-3" onClick={()=>{setText(text.toLowerCase())}}>Convert To Lower</button>
+            <button type="button" className="btn btn-primary mx-2 my-3" onClick={removeSpaces}>Remove Spaces</button>
+            <button type="button" className="btn btn-primary mx-2 my-3" onClick={()=>{summarize(text)}}>Summarize</button>
+            <button type="button" className="btn btn-primary mx-2 my-3" onClick={()=>{gen_Text(text)}}>Generate text</button>
+            <button type="button" className="btn btn-danger mx-2 my-3" onClick={()=>{setText("")}}>Clear</button>
+            <div className="container my-3">
+              <h1>Text Info</h1>
+              <p>Your text has {computeCharaters(text)} characters and {computeWord(text)} words</p>
+              <h2>Preview</h2>
+              <p>{respText.length>0?respText:(text.length>0?text:"Enter your text in text-box")}</p>
+              <button type="button" style={{color: props.mode==="light"?"black":"white" }} className="btn btn-success mx-2 my-3" onClick={copyResponse}>Copy</button>
+            </div>
         </div>
+          
+          </>
     )
 }
 Textform.defaultProps = {
